b6fbd664eb6be7ae760295d0574af1ee7773f170
Tim van Dalen <Tim@timvdalen.nl>
2014-03-24 10:24:08 +0100

    Add some output

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,37 @@
 #include "BVH.hpp"
 
 #include <iostream>
+#include <cstdio>
 
 int main(int argc, char** argv){
-  if(argc == 2){
+	if(argc == 2){
 
-    BVH* bvh = new BVH(argv[1]);
-    int maxSize = 5000;
-    Collisions* collisions = new Collisions(bvh, maxSize);
+		BVH* bvh = new BVH(argv[1]);
+		int maxSize = 5000;
+		Collisions* collisions = new Collisions(bvh, maxSize);
 
-    Vector displ;
-    displ.x = displ.y = displ.z = 0;
+		Vector displ;
+		displ.x = displ.y = displ.z = 0;
 
-    bvh->checkCollisions(collisions, displ);
-    
-    delete collisions;
-    delete bvh;
-  }else{
-    std::cout << "Please specify an input file." << std::endl;
-  }
-  return 0;
+		int sum = 0;
+		int arr_length = collisions->nVertices;
+		for(int i=0; i < arr_length; i++){
+			sum += collisions->nPotentialVertexFaces[i];
+		}
+		printf("Before: %d, %d\n", arr_length, sum);
+
+		bvh->checkCollisions(collisions, displ);
+
+		sum = 0;
+		for(int i=0; i < arr_length; i++){
+			sum += collisions->nPotentialVertexFaces[i];
+		}
+		printf("After:%d, %d\n", arr_length, sum);
+	
+		delete collisions;
+		delete bvh;
+	}else{
+		std::cout << "Please specify an input file." << std::endl;
+	}
+	return 0;
 }
